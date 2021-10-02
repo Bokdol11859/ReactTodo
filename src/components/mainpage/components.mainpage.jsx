2fad3be78ff630e8827d5aa4181cb06ad179e1b0
Bokdol11859 <2019147551@yonsei.ac.kr>
2021-10-02 17:06:30 +0900

    finished porting, localstorage implementation required

--- src/components/mainpage/components.mainpage.jsx
+++ src/components/mainpage/components.mainpage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import TodoList from "../lists/components.lists.todolist";
-import DoneList from "../lists/components.lists.donelist";
+import { TodoList } from "../lists/components.lists.todolist";
 import "./components.mainpage.css";
 
 function Mainpage() {
@@ -8,17 +7,6 @@ function Mainpage() {
     <div className="container">
       <h1>오늘 해야할 일이 뭐가 있을까 ?</h1>
       <TodoList />
-      <DoneList />
-      <form className="inputForm">
-        <input
-          name="inputText"
-          className="inputText"
-          type="text"
-          placeholder="할게 뭐가 있지?"
-          autoFocus
-          onSubmit
-        />
-      </form>
     </div>
   );
 }
